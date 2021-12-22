0cabcac170e38834f98082ab4b955ce51c25ae6f
Gois <matheusgoislimasilva@gmail.com>
2021-12-21 23:14:17 -0300

    Initial Commit

---
+++ Shared/handler_idApp.swift
@@ -0,0 +1,17 @@
+//
+//  handler_idApp.swift
+//  Shared
+//
+//  Created by Matheus Gois on 21/12/21.
+//
+
+import SwiftUI
+
+@main
+struct handler_idApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
